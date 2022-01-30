e74fd305a7dd97c07a0ccf06e3c878f5aa805fe9
Tawhid-Komol <tawhidflutter21@gmail.com>
2022-01-31 00:00:01 +0600

    second

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: const EdgeInsets.only(left: 20.0),
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.white,
+            child: const Text('Hello'),
+          ),
+        ),
       ),
     );
   }
