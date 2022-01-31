6b2ed3090dd0faaf610a25ca154f6bfb1fdb76a7
Tawhid-Komol <tawhidflutter21@gmail.com>
2022-02-01 04:24:51 +0600

    third

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # mi_card_flutter
 
-A new Flutter project.
+This is My business card app made with flutter
 
-## Getting Started
+## Project Screenshots
 
-This project is a starting point for a Flutter application.
+![App Screenshot](https://github.com/TawhidKomol/i_am_rich/blob/main/images/Screenshot%202022-01-10%20094815.png?raw=true)
+
+
+##  My Profile Links
+[![portfolio](https://img.shields.io/badge/portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://tawhidkomol.github.io/Portfolio/index.html)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/tawhid-komol-88a43b1a0//)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/TawhidKomol/)
+[![facebook](https://img.shields.io/badge/facebook-1DA1F2?style=for-the-badge&logo=facebook&logoColor=white)](https://www.facebook.com/towhid.komol/)
+[![facebook](https://img.shields.io/badge/instagram-1DA1F2?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/towhid_komol/)
+
 
 A few resources to get you started if this is your first Flutter project:
 
--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,83 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: const Color(0xffffffff),
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: const EdgeInsets.only(left: 20.0),
-            padding: const EdgeInsets.all(10.0),
-            color: Colors.white,
-            child: const Text('Hello'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircleAvatar(
+                radius: 70.0,
+                backgroundImage: AssetImage('images/komol.jpg'),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Towhid Komol',
+                style: TextStyle(
+                  fontFamily: 'DancingScript',
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 25.0,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.black87,
+                  thickness: 1,
+                ),
+              ),
+              Card(
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    '+880 1864-627419',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 18.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    'tawhidkomol.cse@gmail',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
